e3248564552ee3cacd1e7e54d9efc02b91892056
David A Pears <davidapears@gmail.com>
2018-06-18 09:38:54 +0100

    Starting to add seed data

--- db/library.sql
+++ db/library.sql
@@ -1,21 +1,39 @@
--- DROP TABLE loans;
--- DROP TABLE books;
--- DROP TABLE members;
---
--- CREATE TABLE books
--- (
---   id SERIAL8 primary key,
---
--- );
---
--- CREATE TABLE members
--- (
---   id SERIAL8 primary key,
---
--- );
---
--- CREATE TABLE loans
--- (
---   id SERIAL8 primary key,
---
--- );
+DROP TABLE loans;
+DROP TABLE books;
+DROP TABLE members;
+DROP TABLE genres;
+
+
+CREATE TABLE genres
+(
+  id SERIAL8 primary key,
+  genre VARCHAR(255)
+);
+
+CREATE TABLE books
+(
+  id SERIAL8 primary key,
+  title VARCHAR(255),
+  author VARCHAR(255),
+  genre_id INT8 references genres(id)
+);
+
+CREATE TABLE members
+(
+  id SERIAL8 primary key,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  post_code VARCHAR(255),
+  email_address VARCHAR(255),
+  active_membership BOOLEAN
+);
+
+CREATE TABLE loans
+(
+  id SERIAL8 primary key,
+  member_id INT8 references members(id),
+  book_id INT8 references books(id),
+  loan_start_date DATE,
+  loan_length INT8,
+  book_returned BOOLEAN
+);
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,79 @@
-# require_relative( "../models/bok.rb" )
-# require_relative( "../models/member.rb" )
-# require_relative( "../models/loan.rb" )
-# require("pry")
-# 
+require_relative( "../models/genre.rb" )
+require_relative( "../models/book.rb" )
+require_relative( "../models/member.rb" )
+require_relative( "../models/loan.rb" )
+require("pry")
+#
+# loan.delete_all()
+# book.delete_all()
+# member.delete_all()
+
+genre1 = Genre.new({
+  "genre" => "Historical Fiction"
+})
+
+genre1.save()
+
+genre2 = Genre.new({
+  "genre" => "Fantasy Fiction"
+})
+
+genre2.save()
+
+book1 = Book.new({
+  "title" => "The Great Gatsby",
+  "author" => "F Scott Fitzgerald"
+  "genre_id" => genre1.id
+})
+
+book1.save()
+
+book2 = Book.new({
+  "title" => "Harry Potter",
+  "author" => "J K Rowling"
+  "genre_id" => genre1.id
+})
+
+book2.save()
+
+member1 = Member.new({
+  "first_name" => "David",
+  "last_name" => "Pears",
+  "post_code" => "EH8 8EY",
+  "email_address" => "davidapears@gmail.com",
+  "active_membership" => true
+})
+
+member1.save()
+
+member2 = Member.new({
+  "first_name" => "Fred",
+  "last_name" => "Bloggs",
+  "post_code" => "EH7 1XL",
+  "email_address" => "fredblogs@hotmail.com",
+  "active_membership" => false
+})
+
+member2.save()
+
+loan1 = Loan.new({
+  "member_id" => member1.id,
+  "book_id" => book1,
+  "loan_start_date" => Time.now
+  "loan_length" => 14
+  "book_returned" => false
+})
+
+loan1.save()
+
+loan2 = Loan.new({
+  "member_id" => member2.id,
+  "book_id" => book2,
+  "loan_start_date" => Time.now
+  "loan_length" => 14
+  "book_returned" => false
+})
+
+
+binding.pry
+nil
